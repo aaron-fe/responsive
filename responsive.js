68051b8a4e95c06caf39a6ae042b75b518892066
aaron-fe <xiaohong4512@163.com>
2013-12-20 13:29:35 +0800

    Create responsive.js

---
+++ responsive.js
@@ -0,0 +1,43 @@
+/**
+ * by lizhuhong (xiaohong4512@163.com)
+ * A responsive design solution  
+ * for browsers not support media query, like IE8- 
+ * jQuery is required
+ * add a class to html tag
+ */
+
+(function(w){
+	var $w = $(w),
+		responseSizes = [990, 1200, 1600], //all the sizes need to be responsed
+		minScreenWidth = 0;     //minimum screen size
+		maxScreenWidth = 5000;  //maximum screen size
+
+	function resizeDoc() {
+		var ww = $w.width(),
+			$html = $('html'),
+			len = responseSizes.length,
+			i = 0,
+			prevWidth,
+			curWidth;
+		
+		for (; i <= len; i++) {
+			prevWidth = (i !== 0 ? responseSizes[i - 1] : minScreenWidth);
+			curWidth = (i !== len ? responseSizes[i] : maxScreenWidth);
+
+			if (i !== len) {
+				$html.removeClass('w' + curWidth);
+			}
+			
+			if (prevWidth < ww && ww <= curWidth) {
+				i == 0 ? $html.removeClass('w' + prevWidth) : $html.addClass('w' + prevWidth);
+			}
+		}
+	}
+
+	resizeDoc();
+
+	$w.on('resize', function(e) {
+		resizeDoc();
+	});	
+
+})(window);
