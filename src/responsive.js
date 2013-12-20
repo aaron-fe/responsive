619e4d1f6bef434cfcf95b8f39e9b16daddb3138
aaron-fe <xiaohong4512@163.com>
2013-12-20 13:42:52 +0800

    change tab indent to 2

--- src/responsive.js
+++ src/responsive.js
@@ -7,37 +7,36 @@
  */
 
 (function(w){
-	var $w = $(w),
-		responseSizes = [990, 1200, 1600], //all the sizes need to be responsed
-		minScreenWidth = 0;                //minimum screen size
-		maxScreenWidth = 5000;             //maximum screen size
+  var $w = $(w),
+      responseSizes = [990, 1200, 1600], //all the sizes need to be responsed
+      minScreenWidth = 0;                //minimum screen size
+      maxScreenWidth = 5000;             //maximum screen size
 			
-	function resizeDoc() {
-		var ww = $w.width(),
-				$html = $('html'),
-				len = responseSizes.length,
-				i = 0,
-				prevWidth,
-				curWidth;
+  function resizeDoc() {
+    var ww = $w.width(),
+        $html = $('html'),
+        len = responseSizes.length,
+        i = 0,
+        prevWidth,
+        curWidth;
 		
-		for (; i <= len; i++) {
-			prevWidth = (i !== 0 ? responseSizes[i - 1] : minScreenWidth);
-			curWidth = (i !== len ? responseSizes[i] : maxScreenWidth);
+    for (; i <= len; i++) {
+      prevWidth = (i !== 0 ? responseSizes[i - 1] : minScreenWidth);
+      curWidth = (i !== len ? responseSizes[i] : maxScreenWidth);
 
-			if (i !== len) {
-				$html.removeClass('w' + curWidth);
-			}
+      if (i !== len) {
+        $html.removeClass('w' + curWidth);
+      }
 			
-			if (prevWidth < ww && ww <= curWidth) {
-				i == 0 ? $html.removeClass('w' + prevWidth) : $html.addClass('w' + prevWidth);
-			}
-		}
-	} 
+      if (prevWidth < ww && ww <= curWidth) {
+        i == 0 ? $html.removeClass('w' + prevWidth) : $html.addClass('w' + prevWidth);
+      }
+    }
+  } 
 
-	resizeDoc();
-
-	$w.on('resize', function(e) {
-		resizeDoc();
-	});	
+  resizeDoc();
+  $w.on('resize', function(e) {
+    resizeDoc();
+  });	
 
 })(window);
